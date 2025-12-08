fc2f13d4ad94377d21cdce6c7970f66c54482f66
WorkSpot-JP <asao.kamei@gmail.com>
2025-12-08 22:30:53 +0900

    Enhance interfaces with improved documentation and new methods.

--- src/HydratorInterface.php
+++ src/HydratorInterface.php
@@ -18,12 +18,25 @@ interface HydratorInterface
      */
     public function getTableName(): string;
     /**
-     * DB primary key
+     * Name of the identifier property.
      */
     public function getPrimaryKey(): string;
 
+    /**
+     * Name of the DB's primary key column.
+     */
     public function getPrimaryKeyColumn(): string;
+
+    /**
+     * Name of the DB's column name for the createdAt.
+     * Returns null if not present.
+     */
     public function getCreatedAtColumn(): ?string;
+
+    /**
+     * Name of the DB's column name for the updatedAt.
+     * * returns null if not present.
+     */
     public function getUpdatedAtColumn(): ?string;
 
     /**
@@ -32,12 +45,12 @@ interface HydratorInterface
     public function listProperties(): array;
 
     /**
-     * Column name for creation timestamp
+     * Property name for creation timestamp
      */
     public function getCreatedAt(): ?string;
 
     /**
-     * Column name for update timestamp
+     * Property name for update timestamp
      */
     public function getUpdatedAt(): ?string;
 
@@ -52,7 +65,7 @@ interface HydratorInterface
     public function dehydrate(EntityInterface $entity): array;
 
     /**
-     * Get property name for a given column name
+     * Get a property name for a given column name
      * If column name mapping is not available, returns the column name as-is
      */
     public function getPropertyNameForColumn(string $columnName): ?string;
--- src/RepositoryInterface.php
+++ src/RepositoryInterface.php
@@ -10,12 +10,20 @@ use PDOStatement;
  */
 interface RepositoryInterface
 {
+    /**
+     * @return PDO
+     */
     public function getDb(): PDO;
 
+    /**
+     * returns the database table name of this repository.
+     *
+     * @return string
+     */
     public function getTableName(): string;
 
     /**
-     * executes sql with data and returns results as PDOStatement.
+     * executes the sql with data and returns results as PDOStatement.
      *
      * @param string $sql
      * @param array $data
@@ -25,7 +33,7 @@ interface RepositoryInterface
     public function execute(string $sql, array $data): false|PDOStatement;
 
     /**
-     * fetches entities from sql and data.
+     * executes the sql with data and returns results as entities.
      * 
      * @param string $sql
      * @param array $data
@@ -44,7 +52,7 @@ interface RepositoryInterface
     public function find(int|string $id, string $column = null, string $orderBy = null): array;
 
     /**
-     * get array of column and property names;
+     * get an array of column and property names;
      *
      * @return array<string,string>   array of column name to property name mapping (e.g. ['user_id' => 'id', 'user_name' => 'name'])
      */
